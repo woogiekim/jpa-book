ee380574e7137e53d6698760d1199e28c041eaf2
Woogie Kim <mdir2kor@gmail.com>
2022-01-01 02:22:19 +0900

    chore: 설정추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.8"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
     kotlin("jvm") version "1.5.32"
     kotlin("plugin.spring") version "1.5.32"
     kotlin("plugin.jpa") version "1.5.32"
+    kotlin("plugin.allopen") version "1.5.32"
 }
 
 group = "jpabook"
@@ -23,7 +25,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/jpabook/jpashop/JpaShopApplication.kt
+++ src/main/kotlin/jpabook/jpashop/JpaShopApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class JpashopApplication
+class JpaShopApplication
 
 fun main(args: Array<String>) {
-    runApplication<JpashopApplication>(*args)
+    runApplication<JpaShopApplication>(*args)
 }
